3903743eefe9dcd9c38a4f83f780bc93db4fcf46
liqMix <liqmix@protonmail.com>
2025-08-23 17:01:02 -0600

    increase allowable lane speeds

--- internal/state/settings.go
+++ internal/state/settings.go
@@ -362,7 +362,7 @@ func (s *Settings) createGameplayOptions(group *ui.UIGroup) {
 	optionPos.Y += optionsOffset
 
 	// Lane Speed
-	laneSpeeds := []float64{0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0}
+	laneSpeeds := []float64{0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0}
 	currentSpeedIdx := 0
 	for i, speed := range laneSpeeds {
 		if user.S().LaneSpeed == speed {
